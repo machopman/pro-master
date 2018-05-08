124a319060e0ba4bc4f4b58e9488f5b523d5e131
macho <machopman_shop@hotmail.com>
2018-05-08 12:09:13 +0700

    ijujlkl

--- test.py
+++ test.py
@@ -72,165 +72,156 @@ def webhook():
     return 'OK'
 
 @handler.add(MessageEvent, message=TextMessage)
-
 def movie(event):
     question = event.message.text
     movie = checkd(question)
     movie = checkques(movie)
-    moviename = re.sub('[abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890]', '', movie).replace(' ', '')
-    if moviename !='':
-        cut = mmcut(movie)
-        words = []
-        for row in cut:
-            words.append(row)
-        max = 15
-        word = ''
-        for line in words:
-            cut_len = len(line) - 1
-            # print(cut)
-            if cut_len >= max:
-                max = cut_len
+    cut = mmcut(movie)
+    words = []
+    for row in cut:
+        words.append(row)
+    max = 15
+    word = ''
+    for line in words:
+        cut_len = len(line) - 1
+        # print(cut)
+        if cut_len >= max:
+            max = cut_len
 
-        cut_len = len(cut)
-        count1 = max - cut_len
-        # word = []
-        for line2 in range(count1):
-            str = 'PAD'
-            words.append(str)
-        inputs = {'input': [words], 'cate': [['0', '0', '0', '0', '0', '0', '0', '0', '0', '0']]}
-        inputs2 = []
-        for poemCount in range(len(inputs['input'])):
-            poem = []
-            for count in range(15):
-                search = inputs['input'][poemCount][count]
-                search = search.replace('\ufeff', '')
+    cut_len = len(cut)
+    count1 = max - cut_len
+    # word = []
+    for line2 in range(count1):
+        str = 'PAD'
+        words.append(str)
+    inputs = {'input': [words], 'cate': [['0', '0', '0', '0', '0', '0', '0', '0', '0', '0']]}
+    inputs2 = []
+    for poemCount in range(len(inputs['input'])):
+        poem = []
+        for count in range(15):
+            search = inputs['input'][poemCount][count]
+            search = search.replace('\ufeff', '')
 
-                if search != 'PAD':
-                    # print(search)
-                    try:
-                        index = dictionary[search]
-                        # [i for i, word in reverse_dictionary.items() if word == search]
-                        if index != None:
+            if search != 'PAD':
+                # print(search)
+                try:
+                    index = dictionary[search]
+                    # [i for i, word in reverse_dictionary.items() if word == search]
+                    if index != None:
 
-                            # print(index)
-                            poem = np.concatenate((poem, final_embeddings[index]))
-                            # print(poem.shape)
-                        else:
-                            print(search)
-                            # print(poem)
-                    except:
-                        text = 'ไม่รู้เลยจ้า'
-                        print(text)
-                else:
-                    poem = np.concatenate((poem, np.zeros(17)))
-            inputs2.append(poem)
-        graph = tf.Graph()
-        with graph.as_default():
-            wordCount = 15
-            cateDimension = 10
-            weightColumn1 = 17
-            weightColumn2 = 64
-            weightRow = 3
-            features1 = 64
-            features2 = 64
+                        # print(index)
+                        poem = np.concatenate((poem, final_embeddings[index]))
+                        # print(poem.shape)
+                    else:
+                        print(search)
+                        # print(poem)
+                except:
+                    text = 'ไม่รู้เลยจ้า'
+                    print(text)
+            else:
+                poem = np.concatenate((poem, np.zeros(17)))
+        inputs2.append(poem)
+    graph = tf.Graph()
+    with graph.as_default():
+        wordCount = 15
+        cateDimension = 10
+        weightColumn1 = 17
+        weightColumn2 = 64
+        weightRow = 3
+        features1 = 64
+        features2 = 64
 
-            sess = tf.InteractiveSession()
-            pooled_outputs = []
-            x = tf.placeholder(tf.float32, shape=[None, wordCount * weightColumn1]) #inputvec
-            y_ = tf.placeholder(tf.float32, shape=[None, cateDimension])    #outputvec
-            # y_ = tf.placeholder(tf.float32, shape=[None, cateDimension])
-            for i, filter_size in enumerate([2, 3, 4]):
-                l2_loss = tf.constant(0.0)
-                l2_reg_lambda = 0.0
-                with tf.name_scope("conv-maxpool-%s" % filter_size):
-                    # Convolution Layer
-                    filter_shape = [filter_size, weightColumn1, 1, features1]
-                    W = tf.Variable(tf.truncated_normal(filter_shape, stddev=0.1), name="W")
-                    b = tf.Variable(tf.constant(0.1, shape=[features1]), name="b")
+        sess = tf.InteractiveSession()
+        pooled_outputs = []
+        x = tf.placeholder(tf.float32, shape=[None, wordCount * weightColumn1])
+        y_ = tf.placeholder(tf.float32, shape=[None, cateDimension])
+        # y_ = tf.placeholder(tf.float32, shape=[None, cateDimension])
+        for i, filter_size in enumerate([2, 3, 4]):
+            l2_loss = tf.constant(0.0)
+            l2_reg_lambda = 0.0
+            with tf.name_scope("conv-maxpool-%s" % filter_size):
+                # Convolution Layer
+                filter_shape = [filter_size, weightColumn1, 1, features1]
+                W = tf.Variable(tf.truncated_normal(filter_shape, stddev=0.1), name="W")
+                b = tf.Variable(tf.constant(0.1, shape=[features1]), name="b")
 
-                    # print(x.get_shape())
-                    # print(W.get_shape())
-                    x_image = tf.reshape(x, [-1, wordCount, weightColumn1, 1])
-                    conv = tf.nn.conv2d(x_image, W, strides=[1, 1, 1, 1], padding='VALID')
+                # print(x.get_shape())
+                # print(W.get_shape())
+                x_image = tf.reshape(x, [-1, wordCount, weightColumn1, 1])
+                conv = tf.nn.conv2d(x_image, W, strides=[1, 1, 1, 1], padding='VALID')
 
-                    # Apply nonlinearity
+                # Apply nonlinearity
 
-                    h = tf.nn.relu(tf.nn.bias_add(conv, b), name="relu")
-                    # Maxpooling over the outputs
-                    pooled = tf.nn.max_pool(
-                        h,
-                        ksize=[1, 15 - filter_size + 1, 1, 1],
-                        strides=[1, 1, 1, 1],
-                        padding='VALID',
-                        name="pool")
-                    pooled_outputs.append(pooled)
+                h = tf.nn.relu(tf.nn.bias_add(conv, b), name="relu")
+                # Maxpooling over the outputs
+                pooled = tf.nn.max_pool(
+                    h,
+                    ksize=[1, 15 - filter_size + 1, 1, 1],
+                    strides=[1, 1, 1, 1],
+                    padding='VALID',
+                    name="pool")
+                pooled_outputs.append(pooled)
 
-            # Combine all the pooled features
-            num_filters_total = features1 * len([2, 3, 4])
-            print(pooled_outputs)
+        # Combine all the pooled features
+        num_filters_total = features1 * len([2, 3, 4])
+        print(pooled_outputs)
 
-            # h_pool = tf.concat(3, pooled_outputs)
-            h_pool = tf.concat(pooled_outputs, 3)
-            h_pool_flat = tf.reshape(h_pool, [-1, num_filters_total])
+        # h_pool = tf.concat(3, pooled_outputs)
+        h_pool = tf.concat(pooled_outputs, 3)
+        h_pool_flat = tf.reshape(h_pool, [-1, num_filters_total])
 
-            # Add dropout
-            with tf.name_scope("dropout"):
-                keep_prob = tf.placeholder(tf.float32)
+        # Add dropout
+        with tf.name_scope("dropout"):
+            keep_prob = tf.placeholder(tf.float32)
 
-                h_drop = tf.nn.dropout(h_pool_flat, keep_prob)
+            h_drop = tf.nn.dropout(h_pool_flat, keep_prob)
 
-            # Final (unnormalized) scores and predictions
-            with tf.name_scope("output"):
-                W = tf.get_variable(
-                    "W",
-                    shape=[num_filters_total, cateDimension],
-                    initializer=tf.contrib.layers.xavier_initializer())
-                b = tf.Variable(tf.constant(0.1, shape=[cateDimension]), name="b")
-                l2_loss += tf.nn.l2_loss(W)
-                l2_loss += tf.nn.l2_loss(b)
-                # scores = tf.nn.xw_plus_b(h_drop, W, b, name="scores")
-                scores = tf.nn.relu(tf.nn.xw_plus_b(h_drop, W, b, name="scores"))
-                # predictions = tf.argmax(scores, 1, name="predictions")
+        # Final (unnormalized) scores and predictions
+        with tf.name_scope("output"):
+            W = tf.get_variable(
+                "W",
+                shape=[num_filters_total, cateDimension],
+                initializer=tf.contrib.layers.xavier_initializer())
+            b = tf.Variable(tf.constant(0.1, shape=[cateDimension]), name="b")
+            l2_loss += tf.nn.l2_loss(W)
+            l2_loss += tf.nn.l2_loss(b)
+            # scores = tf.nn.xw_plus_b(h_drop, W, b, name="scores")
+            scores = tf.nn.relu(tf.nn.xw_plus_b(h_drop, W, b, name="scores"))
+            # predictions = tf.argmax(scores, 1, name="predictions")
 
-            # CalculateMean cross-entropy loss
-            with tf.name_scope("loss"):
-                losses = tf.nn.softmax_cross_entropy_with_logits(logits=scores, labels=y_)
-                loss = tf.reduce_mean(losses) + l2_reg_lambda * l2_loss
+        # CalculateMean cross-entropy loss
+        with tf.name_scope("loss"):
+            losses = tf.nn.softmax_cross_entropy_with_logits(logits=scores, labels=y_)
+            loss = tf.reduce_mean(losses) + l2_reg_lambda * l2_loss
 
-            correct_prediction = tf.equal(tf.argmax(scores, 1), tf.argmax(y_, 1))
-            train_step = tf.train.AdamOptimizer(1e-4).minimize(losses)  #######///
-            accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))  ########///
+        correct_prediction = tf.equal(tf.argmax(scores, 1), tf.argmax(y_, 1))
+        train_step = tf.train.AdamOptimizer(1e-4).minimize(losses)  #######///
+        accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))  ########///
 
-            saver = tf.train.Saver()  # Gets all variables in `graph`.
-            with tf.Session(graph=graph) as sess:
-                saver.restore(sess, 'model.ckpt')
-                p = sess.run(tf.argmax(scores, 1), feed_dict={x: inputs2, keep_prob: 1.0})
-            print(p)
-            classify = p
+        saver = tf.train.Saver()  # Gets all variables in `graph`.
+        with tf.Session(graph=graph) as sess:
+            saver.restore(sess, 'model.ckpt')
+            p = sess.run(tf.argmax(scores, 1), feed_dict={x: inputs2, keep_prob: 1.0})
+        print(p)
+        classify = p
 
-            user = mongo.db.users
+        user = mongo.db.users
 
-            userid = event.source.user_id
+        userid = event.source.user_id
 
-            name = re.sub('[กขฃคฅฆงจฉชซฌญฎฏฐฑฒณดตถทธนบปผฝพฟภมยรลวศษสหฬอฮฝฦใฬมฒท?ื์ิ.่๋้็เโ,ฯี๊ัํะำไๆ๙๘๗๖๕ึ฿ุู๔๓๒๑+ๅาแ]', '',
-                          question).replace(' ', '')
-            movie_name = searchMovieNameInDic(question)
-            if name != '' and classify != 9:  # คำถามมีชื่อหนัง
-                Type(classify, event, movie_name, userid, user, question, name)
+        name = re.sub('[กขฃคฅฆงจฉชซฌญฎฏฐฑฒณดตถทธนบปผฝพฟภมยรลวศษสหฬอฮฝฦใฬมฒท?ื์ิ.่๋้็เโ,ฯี๊ัํะำไๆ๙๘๗๖๕ึ฿ุู๔๓๒๑+ๅาแ]', '',
+                      question).replace(' ', '')
+        movie_name = searchMovieNameInDic(question)[0]
+        if name != '' and classify != 9:  # คำถามมีชื่อหนัง
+            Type(classify, event, movie_name, userid, user, question, name)
 
-            elif (name == '') and (movie_name != '') and classify != 9:  # คำถาม
-                Type(classify, event, movie_name, userid, user, question, name)
+        elif (name == '') and (movie_name != '') and classify != 9:  # คำถาม
+            Type(classify, event, movie_name, userid, user, question, name)
 
-            elif (name == '') and (movie_name == '') and classify != 9:
-                Type(classify, event, movie_name, userid, user, question, name)
-            elif (name == '') and (movie_name == '') and classify == 9:
-                general(question, event, userid, user)
-    if moviename == '':
-        translator = Translator()
-        translations = translator.translate(question, dest='th')
-        detail  =translations.text
-        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=detail))
-
+        elif (name == '') and (movie_name == '') and classify != 9:
+            Type(classify, event, movie_name, userid, user, question, name)
+        elif (name == '') and (movie_name == '') and classify == 9:
+            general(question, event, userid, user)
 
 def Type(q, event, movie_name,userid,user,question,name):
     if q == 0: #actor
